b957fe184d8b4552d251a58a1e49b7b515e43920
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-26 22:11:05 +0400

    -- DB connection second approach

--- src/main/java/org/alexr/jdbc/step1conn/ConnectionApp.java
+++ src/main/java/org/alexr/jdbc/step1conn/ConnectionApp.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 
 public class ConnectionApp {
   public static void main(String[] args) {
-    Connection conn = DbConnection.getConnection();
+    // connection way # 1
+    Connection conn1 = DbConnection.getConnection();
+    // connection way # 2
+    Connection conn2 = DbConnection2.getConnection();
   }
 }
--- src/main/java/org/alexr/jdbc/step1conn/DbConnection2.java
+++ src/main/java/org/alexr/jdbc/step1conn/DbConnection2.java
@@ -3,20 +3,24 @@ package org.alexr.jdbc.step1conn;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
-public class DbConnection {
+public class DbConnection2 {
   private static final String DB_URL = "jdbc:postgresql://localhost:5432/ibatech";
-  private static final String USERNAME = "postgres";
-  private static final String USER_PASS = "secret";
+  private static final Properties props = new Properties(){{
+    setProperty("user","postgres");
+    setProperty("password","secret");
+    setProperty("ssl","false");
+  }};
 
   private static Connection connection;
 
-  private DbConnection() {}
+  private DbConnection2() {}
 
   public static Connection getConnection() {
     if (connection == null) {
       try {
-        connection = DriverManager.getConnection(DB_URL, USERNAME, USER_PASS);
+        connection = DriverManager.getConnection(DB_URL, props);
       } catch (SQLException e) {
         throw new RuntimeException("Something went wrong during connection", e);
       }
