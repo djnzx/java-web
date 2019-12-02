47d4ea4d29b6e871c99dce4bac90da94564ede3d
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-02 19:55:16 +0400

    -- windows_install.txt

--- src/main/java/org/alexr/web/MinimalWebApplication.java
+++ src/main/java/org/alexr/web/MinimalWebApplication.java
@@ -4,6 +4,9 @@ import org.alexr.web.servlet.HelloServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
+/**
+ * http://localhost:9001
+ */
 public class MinimalWebApplication {
   public static void main(String[] args) throws Exception {
     Server server = new Server(9001);
